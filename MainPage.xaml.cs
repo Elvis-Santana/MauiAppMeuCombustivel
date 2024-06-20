eea56c3563c77707df5f232ecabd061100908baf
Elvis Santana <elvissantanaguimaraes@gmail.com>
2024-06-20 11:31:38 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MauiAppMeuCombustivel;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			double etanl = Convert.ToDouble(txt_etanol.Text);
+            double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+			string msg = "";
+
+			if (etanl <= (gasolina * 0.7))
+				msg = "O etanol está compoensando.";
+			else
+				msg = "O gasolina está compoensando.";
+
+			DisplayAlert("Calculado",msg,"Ok");
+            
+			
+
+        }
+        catch (Exception ex)
+		{
+			DisplayAlert("Ops",ex.Message,"Fechar");
+		}
+    }
+}
+
